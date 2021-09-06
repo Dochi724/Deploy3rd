f22f645b2b573af941e5cbc657aa4f0c014c89cc
dkakeh11 <dkakeh11@naver.com>
2021-09-07 01:18:51 +0900

    SignupAPI

---
+++ server/djangoapi/account/models.py
@@ -0,0 +1,62 @@
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
+
+class UserManager(BaseUserManager):
+    # 일반 user 생성
+    def create_user(self, email, username, favorite_place, password=None):
+        if not email:
+            raise ValueError('must have user email')
+        if not username:
+            raise ValueError('must have user username')
+        if not favorite_place:
+            raise ValueError('must have user favorite_place')
+        user = self.model(
+            email = self.normalize_email(email),
+            username = username,
+            favorite_place = favorite_place,
+        )
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    # 관리자 user 생성
+    def create_superuser(self, email, username, favorite_place, password=None):
+        user = self.create_user(
+            email,
+            password = password,
+            username = username,
+            favorite_place = favorite_place,
+        )
+        user.is_admin = True
+        user.save(using=self._db)
+        return user
+
+class User(AbstractBaseUser):
+    id = models.AutoField(primary_key=True)
+    email = models.EmailField(default='', max_length=100, null=False, blank=False, unique=True)
+    username = models.CharField(default='', max_length=100, null=False, blank=False, unique=True)
+    favorite_place = models.CharField(default='', max_length=100, null=False, blank=False)
+    
+    # User 모델의 필수 field
+    is_active = models.BooleanField(default=True)    
+    is_admin = models.BooleanField(default=False)
+
+    # 헬퍼 클래스 사용
+    objects = UserManager()
+
+    # 사용자의 username field는 nickname으로 설정
+    USERNAME_FIELD = 'username'
+    # 필수로 작성해야하는 field
+    REQUIRED_FIELDS = ['favorite_place', 'email']
+
+    def __str__(self):
+        return self.username
+    def has_perm(self, perm, obj=None):
+        return True
+
+    def has_module_perms(self, app_label):
+        return True
+
+    @property
+    def is_staff(self):
+        return self.is_admin
