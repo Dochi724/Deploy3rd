cdbec89a732100f1f4ea0062bcef930f647babeb
dkakeh11 <dkakeh11@naver.com>
2021-11-17 23:53:29 +0900

    tags

--- server/djangoapi/articles/serializers.py
+++ server/djangoapi/articles/serializers.py
@@ -1,8 +1,8 @@
 
-from .models import Article,Comment,Like, Image
+from .models import Article,Comment,Like, Image, Tag
 
 from rest_framework import serializers
-
+from taggit.serializers import (TagListSerializerField, TaggitSerializer)
 
 class ImageSerializer(serializers.ModelSerializer):
     image = serializers.ImageField(use_url=True)
@@ -10,24 +10,35 @@ class ImageSerializer(serializers.ModelSerializer):
         model = Image
         fields = ['image']
 
-class ArticleListSerializer(serializers.ModelSerializer):
+class TagSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Tag
+        fields = '__all__'
+
+
+class ArticleListSerializer(TaggitSerializer, serializers.ModelSerializer):
 
     images = serializers.SerializerMethodField()
-
+    tags = TagListSerializerField()
     def get_images(self, obj):
         image = obj.image_set.all()
         return ImageSerializer(instance=image, many=True, context=self.context).data
 
     class Meta:
         model = Article
-        fields = ('id', 'title', 'images')
+        fields = ('id', 'title', 'images','tags')
     def create(self, validated_data):
         instance = Article.objects.create(**validated_data)
         image_set = self.context['request'].FILES
-        for image_data in image_set.getlist('image'):
-            Image.objects.create(diary=instance, image=image_data)
-        return instance
 
+        # to_be_tagged, validated_data = self._pop_tags(validated_data)
+        # tag_object = super(TaggitSerializer, self).create(validated_data)
+        # feed = self._save_tags(tag_object, to_be_tagged)
+        # 추후수정
+
+        for image_data in image_set.getlist('image'):
+            Image.objects.create(article=instance, image=image_data)
+        return instance
 class CommentSerializer(serializers.ModelSerializer):
 
     class Meta:
@@ -36,9 +47,10 @@ class CommentSerializer(serializers.ModelSerializer):
         read_only_fields = ('article',) #읽기 전용 필드
 
 
-class ArticleDetailSerializer(serializers.ModelSerializer):
+class ArticleDetailSerializer(TaggitSerializer, serializers.ModelSerializer):
     comment_set = CommentSerializer(many=True, read_only=True)
     images = serializers.SerializerMethodField()
+    tags = TagListSerializerField()
     def get_images(self, obj):
         image = obj.image_set.all()
         return ImageSerializer(instance=image, many=True, context=self.context).data
