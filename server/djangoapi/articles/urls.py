ad53aab2caf3e19a41c278071279428a2392a4be
gimume <yum17201619@gmail.com>
2021-11-25 00:12:22 +0900

    뭔가 달라졌나

--- server/djangoapi/articles/urls.py
+++ server/djangoapi/articles/urls.py
@@ -3,6 +3,7 @@ from . import views
 from rest_framework import urls
 from django.conf.urls.static import static
 from django.conf import settings
+
 urlpatterns =[
     path('', views.article_list),
     path('<int:article_pk>/', views.article_detail),
